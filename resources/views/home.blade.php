9d5c6302f31e22ad0a43380cee73e7cfe1f2b76e
Paula Berto <60474678+paulanberto@users.noreply.github.com>
2025-02-02 23:57:37 +0000

    Atualizacao com prendas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,67 @@
 @extends('layouts.fo_layout')
 @section('content')
-    <h5>Olá estou em casa.</h5>
-    <h6> {{$myVar}}</h6>
-    <h6> {{$contactoInfo['nome']}}</h6>
+    <div class="home">
 
-    <img src="{{asset('image/image1.jpg')}}" alt="" height="200px" width="300px">
-    <ul>
-        <li><a href="{{ route('users.show') }}">Todos os users</a></li>
-        <li><a href="{{ route('users.add') }}">Adicionar Utilizador</a></li>
-        <li><a href="{{ route('tasks') }}">Ver Tarefas</a></li>
-        <li><a href="{{ route('tasks.add') }}">Adicionar Tarefas</a></li>
-        <li><a href="{{ route('gifts.all') }}">Ver Prendas</a></li>
-        <li><a href="{{ route('gifts.add') }}">Adicionar Prendas</a></li>
+        <h5> {{ $myVar }}</h5>
 
-    </ul>
+        <img class="imagem" src="{{ asset('image/dev2.jpg') }}" alt="" height="200px" width="300px">
+
+
+
+        <h6 class="contactoInfo"> Eu sou a {{ $contactoInfo['nome'] }} e desenvolvi este sistema</h6>
+
+
+
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card" style="width: 25rem;">
+                        <img src="{{ asset('image/dev.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Utilizadores</h5>
+                            <p class="card-text">Gestão de utilizadores</p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="{{ route('users.show') }}" class="card-link">Ver todos os
+                                utilizadores</a>
+                            <a href="{{ route('users.add') }}" class="card-link">Adicionar Utilizador</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card" style="width: 25rem;">
+                        <img src="{{ asset('image/tasks.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Tarefas</h5>
+                            <p class="card-text">Gestão de Tarefas</p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="{{ route('tasks') }}" class="card-link">Ver Tarefas</a>
+                            <a href="{{ route('tasks.add') }}" class="card-link">Adicionar Tarefas</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card" style="width: 25rem;">
+                        <img src="{{ asset('image/gift.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Prendas</h5>
+                            <p class="card-text">Gestão de prendas</p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="{{ route('gifts.all') }}" class="card-link">Ver Prendas</a>
+                            <a href="{{ route('gifts.add') }}" class="card-link">Adicionar Prendas</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 @endsection
--- resources/views/layouts/fo_layout.blade.php
+++ resources/views/layouts/fo_layout.blade.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Paula Berto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg custom-navbar bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Paula Berto</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -31,14 +31,14 @@
                         <a class="nav-link" href="{{ route('tasks') }}">Tarefas</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('gifts.all') }}">Gifts</a>
+                        <a class="nav-link" href="{{ route('gifts.all') }}">Prendas</a>
                     </li>
-                    
+
                 </ul>
             </div>
         </div>
     </nav>
-    
+
     <div class="container mt-3">
         @yield('content')
 
